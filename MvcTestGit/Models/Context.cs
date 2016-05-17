f97aaabd184a24cd7dbfc77ad58ba7aaf7810f6f
cakutagawa <c1nti4_@hotmail.com>
2016-05-16 22:53:35 -0300

    product entityframework

---
+++ MvcTestGit/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcTestGit.Models
+{
+    public class Context : DbContext 
+
+    {
+        public DbSet<Product> Produtos { get; set; }
+    }
+}
